6a42d9e24443f63fb7ed61f23f3253c10fa46c67
alpha-acrobat-9971 <qsmity@gmail.com>
2020-03-31 02:54:37 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -4,9 +4,9 @@ var forLoop = function(array){
   if (array === 1){
     loop.push(`I am 1 stange loop.`);
   } else {
-    for (let i = 0, i < 26, i++){
+    for (let i = 0; i < 26; i++){
       loop.push(`I am ${i} strange loops.`);
-    };
+    }
   }
   console.log(loop)
 }
