c66e5d0948731d53018a184e48dcfd00d59459f9
alpha-acrobat-9971 <qsmity@gmail.com>
2020-03-31 03:06:58 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 var loop = []
 
 var forLoop = function(array){
-  for (let i = 1; i < 26; i++){
+  for (let i = 0; i < 25; i++){
     if (i === 1){
       array.push(`I am 1 strange loop.`)
     } else {
